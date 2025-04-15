f9383b1fdb6e4b0c34084d451eba973388c3d3fe
Davronbek Normamatov <davronnormamatov004@gmail.com>
2025-04-15 16:12:24 +0500

    Configured the SecurityFilterChain and added all important security beans, added the expiration time for my tokens

---
+++ src/main/java/com/epam/gym_crm/config/SecurityFilterChainConfig.java
@@ -0,0 +1,47 @@
+package com.epam.gym_crm.config;
+
+import com.epam.gym_crm.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityFilterChainConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private static final String[] WHITE_LIST_URLS = {
+            "/api/v1/trainees/register",
+            "/api/v1/trainers/register",
+            "/api/v1/users/login"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(WHITE_LIST_URLS)
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/auth/logout")
+                                .addLogoutHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                );
+        return http.build();
+    }
+}

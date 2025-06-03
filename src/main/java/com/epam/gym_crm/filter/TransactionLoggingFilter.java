f496977dbd950d4b950c4dd1ce4ff971d2c61aba
Davronbek Normamatov <davronnormamatov004@gmail.com>
2025-06-03 18:14:11 +0500

    improved logging

--- src/main/java/com/epam/gym_crm/filter/TransactionLoggingFilter.java
+++ src/main/java/com/epam/gym_crm/filter/TransactionLoggingFilter.java
@@ -1,38 +1,41 @@
 package com.epam.gym_crm.filter;
 
-import jakarta.servlet.Filter;
+import com.epam.gym_crm.logging_context.TransactionContext;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.MDC;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
-public class TransactionLoggingFilter implements Filter {
+@RequiredArgsConstructor
+public class TransactionLoggingFilter extends OncePerRequestFilter {
 
     private static final String TRANSACTION_ID = "transactionId";
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    private final TransactionContext transactionContext;
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         String transactionId = UUID.randomUUID().toString();
         MDC.put(TRANSACTION_ID, transactionId);
 
+        transactionContext.setTransactionId(transactionId);
+
         try {
-            System.out.printf("Transaction Start: [%s] %s %s%n", transactionId, httpRequest.getMethod(), httpRequest.getRequestURI());
+            System.out.printf("Transaction Start: [%s] %s %s%n", transactionId, request.getMethod(), request.getRequestURI());
 
-            chain.doFilter(request, response);
+            filterChain.doFilter(request, response);
 
-            System.out.printf("Transaction End: [%s] Status: %d%n", transactionId, httpResponse.getStatus());
+            System.out.printf("Transaction End: [%s] Status: %d%n", transactionId, response.getStatus());
         } finally {
             MDC.remove(TRANSACTION_ID);
         }
